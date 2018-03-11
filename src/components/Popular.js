c1cd4941aa59b29e3e673d294cfaf34bbabbea79
Michael Allan <michaelallan@optushome.com.au>
2018-03-11 12:26:44 +1100

    Create Popular component with local state

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 
 class Popular extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: 'All'
+    }
+    this.select = this.select.bind(this)
+  }
+
+  select (lang) {
+    this.setState(() => ({ selected: lang }))
+  }
+
   render () {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
       <ul className="languages">
         {
-          languages.map((language) => <li key={language}>{ language }</li>)
+          languages.map((lang) => (
+            <li
+              key={ lang }
+              style={ lang === this.state.selected ? { color: '#d0021b' } : null }
+              onClick={ this.select.bind(null, lang) }
+            >
+              { lang }
+            </li>
+          ))
         }
       </ul>
     )
