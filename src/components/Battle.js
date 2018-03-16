ea77786ca97877e6f57074260f5f5791507b6978
Michael Allan <michaelallan@optushome.com.au>
2018-03-16 23:49:17 +1100

    Create PlayerPreview component, with reset button

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const PlayerPreview = (props) => (
+  <div>
+    <div className="column">
+      <img className="avatar"
+        src={props.avatar}
+        alt={"Avatar for " + props.avatar}
+      />
+      <h2 className="username">@{props.username}</h2>
+    </div>
+    <button className="reset"
+      onClick={props.onReset.bind(null, props.id)}
+    >
+      Reset
+    </button>
+  </div>
+)
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  onReset: PropTypes.func.isRequired
+}
+
 class PlayerInput extends Component {
   constructor (props) {
     super(props)
@@ -68,10 +92,11 @@ class Battle extends Component {
       },
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
   handleSubmit (id, username) {
-    const num = id === 0 ? 'One' : 'Two'
+    const num = id === 1 ? 'One' : 'Two'
     const player = `player${num}`
 
     let newState = {
@@ -83,25 +108,59 @@ class Battle extends Component {
     this.setState(newState)
   }
 
+  handleReset (id) {
+    const num = id === 1 ? 'One' : 'Two'
+    const player = `player${num}`
+
+    let newState = {
+      [player]: {}
+    }
+    newState[player].username = ''
+    newState[player].image = null
+
+    this.setState(Object.assign(this.state, newState))
+  }
+
   render () {
     const playerOneName = this.state.playerOne.username
     const playerTwoName = this.state.playerTwo.username
+    const playerOneImage = this.state.playerOne.image
+    const playerTwoImage = this.state.playerTwo.image
+
+    console.log('yo', playerOneName)
     return (
       <div>
         <div className="row">
           { !playerOneName &&
               <PlayerInput
-                id={0}
+                id={1}
                 label="Player One"
                 onSubmit={this.handleSubmit}
               />
           }
+          { playerOneName &&
+              <PlayerPreview
+                id={1}
+                avatar={playerOneImage}
+                username={playerOneName}
+                onReset={this.handleReset}
+              />
+          }
           { !playerTwoName &&
               <PlayerInput
-                id={1}
+                id={2}
                 label="Player Two"
                 onSubmit={this.handleSubmit}
-              />}
+              />
+          }
+          { playerTwoName &&
+              <PlayerPreview
+                id={2}
+                avatar={playerTwoImage}
+                username={playerTwoName}
+                onReset={this.handleReset}
+              />
+          }
         </div>
       </div>
     )
