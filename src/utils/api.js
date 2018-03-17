1296d341c08d01d67366d6569847108206c6e76f
Michael Allan <michaelallan@optushome.com.au>
2018-03-17 22:49:16 +1100

    Implement battle api with helper methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,54 @@
 import axios from 'axios'
 
+const getProfile = (username) => (
+  axios.get(`https://api.github.com/users/${username}`)
+    .then(user => user.data)
+)
+
+const getRepos = (username) => (
+  axios.get(`https://api.github.com/users/${username}/repos?per_page=100`)
+    .then(repos => repos.data)
+)
+
+const getStarCount = (repos) => {
+  return repos.reduce(function (count, repo) {
+    return count + repo.stargazers_count
+  }, 0)
+}
+
+const calculateScore = (profile, repos) => {
+  const followers = profile.followers
+  const totalStars = getStarCount(repos)
+  return (followers * 3) + totalStars
+}
+
+const handleError = (err) => {
+  console.warn(err)
+  return null
+}
+
+const getUserData = (player) => (
+  axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(data => {
+    const profile = data[0]
+    const repos = data[1]
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    }
+  })
+)
+
+const sortPlayers = (players) => (
+  players.sort((a, b) => b.score - a.score)
+)
+
 const API = {
+  battle: (players) => axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError),
   fetchPopularLanguages: (lang) => {
     const uri = window.encodeURI(
       `https://api.github.com/search/repositories?q=stars:>1+language:${lang}`
